888362205c2616172343e5d4923a6f118af09437
Jakob Lierman <jakob.lierman@gmail.com>
2021-11-21 12:37:27 +0100

    chore: disable issue-in-scope commitlint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,14 +4,14 @@ module.exports = {
 		{
 			rules: {
 				'issue-in-scope': (input) => [
-					input.scope && input.scope.match(/^((?!([A-Z0-9a-z]{1,10})-?$)#[1-9][0-9]*)$/g),
+					input.scope && input.scope.match(/^(#[1-9][0-9]*)$/g),
 					'Your scope should contain an issue number (ex. feat(#123): description)',
 				],
 			},
 		},
 	],
 	rules: {
-		'scope-empty': [2, 'never'],
-		'issue-in-scope': [2, 'always'],
+		// 'scope-empty': [2, 'never'],
+		// 'issue-in-scope': [2, 'always'],
 	},
 };
