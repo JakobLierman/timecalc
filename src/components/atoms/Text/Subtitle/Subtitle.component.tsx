caf682c414c09a797652f6e4c9af7350242b76da
Jakob Lierman <jakob.lierman@gmail.com>
2021-11-28 23:02:44 +0100

    fix: better component props typing

--- src/components/atoms/Text/Subtitle/Subtitle.component.tsx
+++ src/components/atoms/Text/Subtitle/Subtitle.component.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
-import { TextProps, TextStyle } from 'react-native';
+import { StyleProp, TextProps, TextStyle } from 'react-native';
 
 import { TComponentProps } from '../../../types';
 
 import Styled from './Subtitle.styled';
 
-const Subtitle: FC<TComponentProps<TextProps, TextStyle>> = ({ ...props }) => {
+const Subtitle: FC<TComponentProps<TextProps, StyleProp<TextStyle>>> = ({ ...props }) => {
 	return <Styled.Subtitle {...props} />;
 };
 
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,4 +1,4 @@
 import { PropsWithChildren } from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
-export type TComponentProps<T, E = ViewStyle> = PropsWithChildren<T> & { style?: StyleProp<E> };
+export type TComponentProps<T, E = StyleProp<ViewStyle>> = PropsWithChildren<T> & { style?: E };
